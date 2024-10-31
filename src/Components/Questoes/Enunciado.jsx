f4e4ab0f7fe130040f11bac324e0f7404036cce3
João Henrique <joaohenriquerbueno@gmail.com>
2024-10-31 13:25:29 -0300

    adicionando compenente do corpo da questão que contera o enunciado e as opções

---
+++ src/Components/Questoes/Enunciado.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Enunciado.module.css';
+
+const Enunciado = ({ texto }) => {
+  return (
+    <div className={styles.questionBody}>
+      <p className={styles.questionText}>
+        Conforme estabelecido no artigo 27 da Lei Complementar nº. 011/03, que
+        dispõe sobre o Estatuto dos Servidores Públicos do Município de
+        Amaralina, “Ao entrar em exercício, o servidor nomeado para o cargo de
+        provimento efetivo fica sujeito a um período de estágio probatório de 3
+        (três) anos, com o objetivo de apurar os requisitos necessários à sua
+        confirmação no cargo para o qual foi nomeado”. Nesse sentido, analise as
+        afirmações abaixo sobre os requisitos básicos a serem apurados no
+        estágio probatório.
+      </p>
+    </div>
+  );
+};
+
+export default Enunciado;
--- src/Components/Questoes/Questoes.jsx
+++ src/Components/Questoes/Questoes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from '../Questoes/Questoes.module.css';
 import { useState } from 'react';
 import InfoConcurso from './InfoConcurso';
+import Enunciado from './Enunciado';
 
 const Questoes = () => {
   const [estadoBtn, setEstadoBtn] = useState(true);
@@ -9,6 +10,7 @@ const Questoes = () => {
   return (
     <div className={styles.questionCard}>
       <InfoConcurso />
+      <Enunciado />
     </div>
   );
 };
