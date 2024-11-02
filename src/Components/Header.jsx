e2d64e279597db99aa6858261fed421a19a10077
João Henrique <joaohenriquerbueno@gmail.com>
2024-11-02 13:46:33 -0300

    alterações de estilo feita no header,falta adicionar script para menu mobile

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import styles from './Header.module.css';
 import logo from '../assets/logo/logo1.png';
+import menu from '../assets/menu/menu.svg';
 
 const Header = () => {
   return (
     <header className={styles.navbar}>
       <div className={styles.navbarBrand}>
-        <img src={logo} alt="logo" className={styles.logo} />
+        <a href="#inicio">
+          <img src={logo} alt="logo" className={styles.logo} />
+        </a>
+      </div>
+      <div className={styles.iconMenu}>
+        <img src={menu} alt="iconMenu" />
       </div>
       <nav className={styles.navbarLinks}>
-        <a href="#home" className={styles.navLink}>
-          Início
-        </a>
         <a href="#questions" className={styles.navLink}>
           Questões
         </a>
         <a href="#about" className={styles.navLink}>
           Sobre
         </a>
-        <a href="#login" className={styles.navLink}>
+        <a href="#login" className={`${styles.navLink} ${styles.login}`}>
           Entrar
         </a>
       </nav>
