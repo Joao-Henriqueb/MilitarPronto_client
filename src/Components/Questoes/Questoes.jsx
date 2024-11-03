b6ea524924cf36f5dc73c95bf6b5d2177b4c1207
João Henrique <joaohenriquerbueno@gmail.com>
2024-11-02 23:36:53 -0300

    criado componente de pesquisa e adicionado ao componente de questões

--- src/Components/Questoes/Questoes.jsx
+++ src/Components/Questoes/Questoes.jsx
@@ -3,13 +3,29 @@ import styles from '../Questoes/Questoes.module.css';
 import InfoConcurso from './InfoConcurso';
 import Enunciado from './Enunciado';
 import QuestionFooter from './QuestionFooter';
+import Search from './Search';
 
 const Questoes = () => {
   return (
-    <div className={styles.questionCard}>
-      <InfoConcurso />
-      <Enunciado />
-      <QuestionFooter />
+    <div className={styles.contentGeral}>
+      <div className={styles.contentSearch}>
+        <h1>Questões de Concurso Militares</h1>
+        <Search />
+        <Search />
+        <Search />
+        <Search />
+      </div>
+      <div className={styles.questionCard}>
+        <InfoConcurso />
+        <Enunciado />
+        <QuestionFooter />
+      </div>
+
+      <div className={styles.questionCard}>
+        <InfoConcurso />
+        <Enunciado />
+        <QuestionFooter />
+      </div>
     </div>
   );
 };
---
+++ src/Components/Questoes/Search.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Search.module.css';
+
+const Search = ({ op }) => {
+  return (
+    <div className={styles.customSelect}>
+      <select>
+        <option value="">xxxxxxxxxxxxx</option>
+        <option value="">xxxx</option>
+        <option value="">xxxx</option>
+        <option value="">xxxx</option>
+        <option value="">xxxx</option>
+      </select>
+    </div>
+  );
+};
+
+export default Search;
