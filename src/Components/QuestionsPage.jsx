2f69ddbf1716348e6aa0a52271d65c3276ad60c4
João Henrique <joaohenriquerbueno@gmail.com>
2024-11-13 00:44:24 -0300

    o componente questionPage foi atualizado, foi usado o useMemo para memorizar o objeto option e não causar renderização continua, o useEffect agora pega o token do usuário e passa para o options que e enviado para a api para poder buscar as questões

--- src/Components/QuestionsPage.jsx
+++ src/Components/QuestionsPage.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext, useMemo } from 'react';
 import styles from './QuestionsPage.module.css';
 import EmptyStateMessage from './EmptyStateMessage';
 import FormSearchBd from './FormSearchBd/FormSearchBd';
 import Questao from './Questoes/Questao';
 import useFetch from '../Hooks/useFetch';
+import { AuthContext } from '../context/AuthContext';
 
 const buildUrl = (baseUrl, filters) => {
   const params = new URLSearchParams();
@@ -19,15 +20,30 @@ const buildUrl = (baseUrl, filters) => {
 const QuestionsPage = () => {
   const [selectedFilters, setSelectedFilters] = useState();
   const [dynamicUrl, setDynamicUrl] = useState('');
+  const [token, setToken] = useState(null);
+
+  const { user } = useContext(AuthContext);
+
+  const options = useMemo(
+    () => ({
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    }),
+    [token],
+  );
 
   useEffect(() => {
+    if (user) setToken(user.accessToken);
     if (selectedFilters) {
       setDynamicUrl(
         buildUrl('http://localhost:5000/questoes', selectedFilters),
       );
+      //setToken();
     }
   }, [selectedFilters]);
-  const { data, loading, error } = useFetch(dynamicUrl);
+  const { data, loading, error } = useFetch(dynamicUrl, options);
 
   return (
     <div className={styles.contentGeral}>
